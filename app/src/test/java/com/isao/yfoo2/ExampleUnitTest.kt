75e6ecfcfd6303fd212011ab01381d764e1159d4
Ihor Pakharenko <ihor.p@volpis.com>
2024-06-15 11:55:22 +0200

    CI/CD: name the caching step, remove intentionally failing test

--- app/src/test/java/com/isao/yfoo2/ExampleUnitTest.kt
+++ app/src/test/java/com/isao/yfoo2/ExampleUnitTest.kt
@@ -13,9 +13,4 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
-
-    @Test
-    fun failing_addition() {
-        assertEquals(4, 2 + 2 + 2)
-    }
 }
