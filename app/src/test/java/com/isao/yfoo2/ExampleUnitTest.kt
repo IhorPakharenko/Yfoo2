0ded6077dd0c31aca3c257ba893b07450e73f236
Ihor Pakharenko <ihor.p@volpis.com>
2024-07-28 12:23:47 +0200

    Make ExampleUnitTest runnable

--- app/src/test/java/com/isao/yfoo2/ExampleUnitTest.kt
+++ app/src/test/java/com/isao/yfoo2/ExampleUnitTest.kt
@@ -1,7 +1,7 @@
 package com.isao.yfoo2
 
 import org.junit.Assert.assertEquals
-import org.junit.Test
+import org.junit.jupiter.api.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
