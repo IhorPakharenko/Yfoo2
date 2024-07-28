54757020568b17660575c1e8b7a449994c3e2ead
Ihor Pakharenko <ihor.p@volpis.com>
2024-07-28 12:24:21 +0200

    Comment out a sample Spek test as it messes with Kotest tests

--- app/src/test/java/com/isao/yfoo2/data/repository/FeedImageRepositoryImplTest.kt
+++ app/src/test/java/com/isao/yfoo2/data/repository/FeedImageRepositoryImplTest.kt
@@ -1,18 +1,14 @@
 package com.isao.yfoo2.data.repository
 
-import org.junit.jupiter.api.Assertions.assertTrue
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.gherkin.Feature
-
-class FeedImageRepositoryImplTest : Spek({
-    Feature("A") {
-        Scenario("B") {
-            When("C") {
-
-            }
-            Then("D") {
-                assertTrue(true)
-            }
-        }
-    }
-})
+//class FeedImageRepositoryImplTest : Spek({
+//    Feature("A") {
+//        Scenario("B") {
+//            When("C") {
+//
+//            }
+//            Then("D") {
+//                assertTrue(true)
+//            }
+//        }
+//    }
+//})
