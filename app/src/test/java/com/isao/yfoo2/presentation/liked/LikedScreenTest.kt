8ec8e9f5aff9c09aec9af75d5409f4798d6d79ac
Ihor Pakharenko <ihor.p@volpis.com>
2024-10-03 16:24:07 +0200

    Trying to fix flaky test: iteration 16

--- app/src/test/java/com/isao/yfoo2/presentation/liked/LikedScreenTest.kt
+++ app/src/test/java/com/isao/yfoo2/presentation/liked/LikedScreenTest.kt
@@ -1,14 +1,33 @@
 package com.isao.yfoo2.presentation.liked
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assertAny
+import androidx.compose.ui.test.assertCountEquals
+import androidx.compose.ui.test.filter
+import androidx.compose.ui.test.hasClickAction
+import androidx.compose.ui.test.hasScrollAction
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.ComposeContentTestRule
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.longClick
+import androidx.compose.ui.test.onChildren
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTouchInput
+import com.isao.yfoo2.R
 import com.isao.yfoo2.presentation.liked.composable.LikedScreen
+import com.isao.yfoo2.presentation.liked.model.LikedImageDisplayableDummies.generateLikedImageDisplayables
 import com.isao.yfoo2.utils.KoinRule
+import com.isao.yfoo2.utils.getString
+import com.isao.yfoo2.utils.hasNoText
 import com.isao.yfoo2.utils.printSemantics
+import io.kotest.matchers.collections.shouldHaveSingleElement
 import org.junit.FixMethodOrder
 import org.junit.Rule
 import org.junit.Test
@@ -16,7 +35,9 @@ import org.junit.runner.RunWith
 import org.junit.runners.MethodSorters
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
+import org.robolectric.shadows.ShadowLooper
 
+@OptIn(ExperimentalFoundationApi::class)
 @RunWith(RobolectricTestRunner::class)
 @Config(qualifiers = "w800dp-h1500dp-xxxhdpi")
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -33,6 +54,7 @@ class LikedScreenTest {
     fun `1`() {
         testRule.setContent {
             LazyColumn(Modifier.fillMaxSize()) {
+                stickyHeader { Text("header") }
                 items(10) {
                     Text("Text number $it")
                 }
@@ -45,6 +67,7 @@ class LikedScreenTest {
     fun `2`() {
         testRule.setContent {
             LazyColumn(Modifier.fillMaxSize()) {
+                stickyHeader { Text("header") }
                 items(10) {
                     Text("Text number $it")
                 }
@@ -57,12 +80,95 @@ class LikedScreenTest {
     fun `3`() {
         testRule.setContent {
             LazyColumn(Modifier.fillMaxSize()) {
+                stickyHeader { Text("header") }
                 items(10) {
                     Text("Text number $it")
                 }
             }
         }
         testRule.printSemantics()
+    }
+
+    // A screenshot test already covers this case, so this test is likely unnecessary
+    @Test
+    fun `when loading, show loading placeholder`() {
+        testRule.setContent {
+            LikedScreen(uiState = LikedUiState(isLoading = true), onIntent = {})
+        }
+        testRule.waitForIdle()
+        ShadowLooper.runUiThreadTasksIncludingDelayedTasks()
+        testRule.printSemantics()
+        testRule.onNodeWithContentDescription(getString(R.string.loading)).assertExists()
+    }
+
+    // A screenshot test already covers this case, so this test is likely unnecessary
+    @Test
+    fun `when error, show error bar`() {
+        testRule.setContent {
+            LikedScreen(uiState = LikedUiState(isError = true), onIntent = {})
+        }
+        testRule.waitForIdle()
+        ShadowLooper.runUiThreadTasksIncludingDelayedTasks()
+        testRule.printSemantics()
+
+        testRule.onNodeWithText(getString(R.string.something_went_wrong)).assertExists()
+    }
+
+    // A screenshot test already covers this case, so this test is likely unnecessary
+    @Test
+    fun `when content available, show all content`() {
+        val content = generateLikedImageDisplayables(2)
+        testRule.setContent {
+            LikedScreen(uiState = LikedUiState(items = content), onIntent = {})
+        }
+        testRule.waitForIdle()
+        ShadowLooper.runUiThreadTasksIncludingDelayedTasks()
+        testRule.printSemantics()
+
+        testRule.onNode(hasScrollAction())
+            .onChildren()
+            .filter(hasClickAction())
+            // Sorting button
+            .assertAny(hasText(getString(R.string.added)))
+            // Items + Sorting button
+            .assertCountEquals(content.size + 1)
+    }
+
+    @Test
+    fun `bwhen content available, sorting button is available`() {
+        val content = generateLikedImageDisplayables(4)
+        testRule.setUpComposable(state = LikedUiState(items = content))
+        // Sorting button
+        testRule.waitForIdle()
+        ShadowLooper.runUiThreadTasksIncludingDelayedTasks()
+        testRule.printSemantics()
+
+        testRule.onNode(hasClickAction() and hasText(getString(R.string.added))).performClick()
+
+        intents shouldHaveSingleElement { it is LikedIntent.SetSorting }
+    }
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun `awhen long clicking item, dropdown appears`() {
+        val content = generateLikedImageDisplayables(4)
+        testRule.setContent {
+            LikedScreen(uiState = LikedUiState(items = content), onIntent = {})
+        }
+        testRule.printSemantics()
+        testRule.waitForIdle()
+        ShadowLooper.runUiThreadTasksIncludingDelayedTasks()
+//        testRule.waitUntilAtLeastOneExists(hasClickAction() and hasNoText())
+//        testRule.waitForIdle()
+        testRule.printSemantics()
+        testRule.onAllNodes(hasClickAction() and hasNoText())[0].performTouchInput { longClick() }
+        testRule.onNode(
+            hasText(getString(R.string.image_by, content[0].source.websiteName))
+        ).assertExists()
+        testRule.onNode(
+            hasClickAction()
+                    and hasText(getString(R.string.delete))
+        ).assertExists()
     }
 
     private fun ComposeContentTestRule.setUpComposable(
